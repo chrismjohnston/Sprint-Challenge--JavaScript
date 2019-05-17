57cc68f1454a7ae0ac17df2305877ee0f54f6f2f
Chris Johnston <cmjohnston@gmail.com>
2019-05-17 11:15:11 -0500

    finished advanced arrays in object-arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -247,7 +247,7 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+const largerPopulation = zooAnimals.filter(animal => animal.population < 5);
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -255,7 +255,9 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce(function(totalAnimals, item) {
+  return totalAnimals + item.population;
+}, 0);
 console.log(populationTotal);
 
 /* 
